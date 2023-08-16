8f2b8d4baa386ea98f388bd17b71a231c001a756
Travis Alstrand <travisalstrand@gmail.com>
2023-08-16 14:29:43 -0700

    moved initial active class code to handlePagination

--- js/script.js
+++ js/script.js
@@ -27,29 +27,32 @@ function handlePagination(array) {
     `;
     paginationList.insertAdjacentHTML("beforeend", html);
   }
+  // 3. Add the `active` class to the first button
+  //    Hint: querySelector()
+  paginationList.querySelector("button").classList.add("active");
 }
 
 /* This function will handle calculating how many and which
 authors to show on the current page and dynamically add them */
 
 function showPage(array, page) {
-  // 3. Create a variable to represent which author to start with on the page.
+  // 4. Create a variable to represent which author to start with on the page.
   //    The math should be (the page multiplied by the authorsPerPage) minus the authorsPerPage ????
-  // 4. Create a variable to represent which author to end with on the page.
+  // 5. Create a variable to represent which author to end with on the page.
   //    The math should be (the page multiplied by the authorsPerPage) minus one ????
   const start = page * authorsPerPage - authorsPerPage;
   const end = page * authorsPerPage - 1;
-  // 5. Reset the authorContainer's content to nothing to prevent previous cards staying on the page
+  // 6. Reset the authorContainer's content to nothing to prevent previous cards staying on the page
   authorContainer.innerHTML = "";
 
-  // 6-a. Start a loop to the length of the array's length
-  // 6-b. Inside, create a conditional checking if `i` is...
+  // 7-a. Start a loop to the length of the array's length
+  // 7-b. Inside, create a conditional checking if `i` is...
   //      - greater than or equal to the start variable
   //      - less than or equal to the end variable
-  // 6-c. If true, create a variable storing a template literal of the HTML markup of an author card.
+  // 7-c. If true, create a variable storing a template literal of the HTML markup of an author card.
   //      (see example in index.html lines 17 - 28).
   //      Hint: You'll need to dynamically add each author's information
-  // 6-d. Then add this variable to the authorContainer element
+  // 7-d. Then add this variable to the authorContainer element
   //      Hint: insertAdjacentHTML()
   for (let i = 0; i < array.length; i++) {
     if (i >= start && i <= end) {
@@ -74,11 +77,11 @@ function showPage(array, page) {
 above to change the page & add the `active` class  */
 
 paginationList.addEventListener("click", (e) => {
-  // 7. Create a variable to store the button which currently has the `active` class
+  // 8. Create a variable to store the button which currently has the `active` class
   const activeButton = paginationList.querySelector(".active");
-  // 8-a. Make sure the user has clicked a `button`
+  // 9-a. Make sure the user has clicked a `button`
   //      Hint: e.target
-  // 8-b. If true...
+  // 9-b. If true...
   //      - Remove the `active` class from the currently active button
   //      - Add the `active` class to the button just clicked
   //      - Call showPage() passing it `authors` and the content of the button just clicked.
@@ -95,9 +98,7 @@ paginationList.addEventListener("click", (e) => {
   }
 });
 
-/* These function calls are needed to initialize the page 
-and add the `active class` to the first button */
+/* These function calls are needed to initialize the page */
 
 handlePagination(authors);
 showPage(authors, 1);
-paginationList.querySelector("button").classList.add("active");
